08fa5013dba56889c8a58cbf7e5fa1f3c2b08841
veenashree97 <veenashree.k@namastecredit.com>
2020-03-29 22:44:26 +0530

    bar graph added

---
+++ src/BarGraph/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import FusionCharts from "fusioncharts";
+import charts from "fusioncharts/fusioncharts.charts";
+import ReactFusioncharts from "react-fusioncharts";
+
+// Resolves charts dependancy
+charts(FusionCharts);
+function BarGraph(props) {
+  const dataSource = {
+    chart: {
+      caption: "Based on country",
+      subcaption: "For the year 2020",
+      yaxisname: "Users count",
+      decimals: "1",
+      theme: "fusion"
+    },
+    data: props.data
+  };
+  return (
+    <ReactFusioncharts
+      type="column3d"
+      width={window.screen.width > 768 ? "40%" : "100%"}
+      height={window.screen.width > 768 ? "45%" : "50%"}
+      dataFormat="JSON"
+      dataSource={dataSource}
+    />
+  );
+}
+
+export default React.memo(BarGraph);

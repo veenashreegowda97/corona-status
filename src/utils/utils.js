b54a0dd2add40113e73dde718f47e8d8c5367330
veenashree97 <veenashree.k@namastecredit.com>
2020-03-29 22:45:21 +0530

    utils file added

---
+++ src/utils/utils.js
@@ -0,0 +1,18 @@
+export const casesColor = [
+  {
+    name: "Confirmed",
+    colour: "yellow"
+  },
+  {
+    name: "Death",
+    colour: "red"
+  },
+  {
+    name: "Recovered",
+    colour: "green"
+  },
+  {
+    name: "Total",
+    colour: "blue"
+  }
+];
